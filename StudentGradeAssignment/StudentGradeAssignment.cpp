bf5f39222904ac4e78ad7110158bd73d3d90b864
Emmanuel Alfred <161465931+emmanuelaalfred@users.noreply.github.com>
2024-02-29 11:59:35 +0000

    Part one answer

--- StudentGradeAssignment/StudentGradeAssignment.cpp
+++ StudentGradeAssignment/StudentGradeAssignment.cpp
@@ -4,39 +4,51 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	char grades[5];
-	FillList(grades, 5);
+// Function prototypes
+void FillList(char grades[], int size);
+char GetLowestGrade(char grades[], int size);
+void UpGradesByOne(char grades[], int size);
+int Main();
 
-	for (int index = 0; index < 5; index++)
+int Main()
+{
+	const int size = 6;
+	char grades[size];
+	FillList(grades, size);
+
+	for (int index = 0; index < size; index++)
 		cout << grades[index] << endl;
 
-	char lowest = GetLowestGrade(grades, 5);
+	char lowest = GetLowestGrade(grades, size);
 
 	cout << endl << lowest << "is the lowest grade \n" << endl;
 
-	UpGradesByOne(grades, 5);
+	UpGradesByOne(grades, size);
 
 	cout << "\nrevised grades are: " << endl;
 
-	for (int index = 0; index < 5; index++)
+	for (int index = 0; index < size; index++)
 		cout << grades[index] << endl;
 
 	return 0;
 }
 
+/// <summary>
+/// Fill an array of grades
+/// </summary>
+/// <param name="grades"></param>
+/// <param name="size"></param>
 void FillList(char grades[], int size) {
-	// write function definition here to populate the array
-	  
-	
-	// with five character values (capital letters only)
-}
+	cout << "Please enter " << size << " grades." << endl;
+	for (int index = 0; index < size; index++)
+		cin >> grades[index];
+	}
 
 
 char GetLowestGrade(char grades[], int size) {
 	// write function definition here to return the lowest
 	// grade in the array (Note: F is Lower than A)
+	return 'F';
 }
 
 void UpGradesByOne(char grades[], int size) {
